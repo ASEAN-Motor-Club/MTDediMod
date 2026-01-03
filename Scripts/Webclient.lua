b0d10aff6c78073b8937956e7cc900de91a778d0
Freeman <4031216+fmnxl@users.noreply.github.com>
2026-01-03 16:23:06 +0700

    refactor: use LoopInGameThreadWithDelay

--- Scripts/Webclient.lua
+++ Scripts/Webclient.lua
@@ -136,7 +136,7 @@ end
 -- This will slot in between webserver loops.
 local delay = (tonumber(os.getenv("MOD_SERVER_PROCESS_AMOUNT")) or 5) * 100
 if delay > 0 and webhookEvents[1] ~= "none" and webhookUrl then
-    LoopAsync(delay, function()
+    LoopInGameThreadWithDelay(delay, function()
         if #requests > 0 then
             local payloads = {} ---@type table[]
             local callbacks = {} ---@type fun(status: boolean)[]
