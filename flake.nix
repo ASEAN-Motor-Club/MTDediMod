f9dcac5563453aa4f8f8ad24206696f476747350
Freeman <4031216+fmnxl@users.noreply.github.com>
2026-04-22 19:59:28 +0700

    deploy(test): MTDediMod server-v0.38.0-rc13 to amc-peripheral

--- flake.nix
+++ flake.nix
@@ -795,7 +795,7 @@
                 maxFps = 30;
                 restartSchedule = "3000-01-01 00:00:00";
                 betaBranch = "beta";
-                modVersion = "server-v0.38.0-rc11";
+                modVersion = "server-v0.38.0-rc13";
                 enableExternalMods = {
                   CarPartsImport_P = false;
                   MoneyRun_P = false;
